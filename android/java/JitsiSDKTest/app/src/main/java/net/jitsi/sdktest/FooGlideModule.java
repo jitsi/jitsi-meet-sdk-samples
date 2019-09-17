2507d0370059efe381a551930687fdd104aaf3f2
Saúl Ibarra Corretgé <s@saghul.net>
2019-09-17 14:59:04 +0200

    android: update example for SDK 2.3.0

--- android/java/JitsiSDKTest/app/src/main/java/net/jitsi/sdktest/FooGlideModule.java
+++
@@ -1,9 +0,0 @@
-package net.jitsi.sdktest;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-
-@GlideModule
-public final class FooGlideModule extends AppGlideModule {
-}
