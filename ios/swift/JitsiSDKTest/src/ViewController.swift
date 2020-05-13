4357da6270c96f2b4e9b2b14d67aa4ccfb37520b
Saúl Ibarra Corretgé <s@saghul.net>
2020-05-13 10:27:31 +0200

    ios: update samples to latest SDK

--- ios/swift/JitsiSDKTest/src/ViewController.swift
+++ ios/swift/JitsiSDKTest/src/ViewController.swift
@@ -1,6 +1,5 @@
 /*
- * Copyright @ 2019-present 8x8, Inc.
- * Copyright @ 2017-2018 Atlassian Pty Ltd
+ * Copyright @ 2017-present 8x8, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
