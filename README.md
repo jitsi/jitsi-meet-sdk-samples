18c35f7625b38233579ff34f761f4c126ba7e03a
Alex Bumbu <alex.bumbu@8x8.com>
2021-06-04 12:22:14 +0300

    ios: add screen sharing example

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ as follows:
 * [ios/objc](ios/objc): iOS examples, written in Objective-C
 * [ios/swift](ios/swift): iOS example, written in Swift
 * [ios/swift-pip](ios/swift-pip): iOS example, written in Swift and showing how Picture-in-Picture can be implemented
+* [ios/swift-screensharing](ios/swift-screensharing): iOS example, written in Swift and showing how Screen Sharing can be implemented
 
 NOTE: You must be using Xcode >= 12.2 for the iOS examples to run.
 
---
+++ ios/swift-screensharing/JitsiSDKScreenSharingTest/JitsiSDKScreenSharingTest/JitsiMeetViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  JitsiSDKTest
+//
+//  Created by Alex-Dan Bumbu on 17.05.2021.
+//
+
+import UIKit
+import JitsiMeetSDK
+
+class JitsiMeetViewController: UIViewController {
+    
+    var meetingName: String!
+    
+    @IBOutlet private var meetView: JitsiMeetView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        meetView.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        joinMeeting(name: meetingName)
+    }
+    
+    private func joinMeeting(name: String) {
+        let options = JitsiMeetConferenceOptions.fromBuilder { builder in
+            builder.room = name
+            builder.audioMuted = true
+            builder.videoMuted = true
+            builder.userInfo = JitsiMeetUserInfo(displayName: "Test User", andEmail: nil, andAvatar: nil)
+            builder.setFeatureFlag("ios.screensharing.enabled", withBoolean: true)
+        }
+        
+        meetView.join(options)
+    }
+}
+
+extension JitsiMeetViewController: JitsiMeetViewDelegate {
+    
+    func conferenceTerminated(_ data: [AnyHashable : Any]!) {
+        dismiss(animated: true)
+    }
+}
+
