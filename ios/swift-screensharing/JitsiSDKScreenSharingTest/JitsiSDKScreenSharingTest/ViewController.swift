c2a65831c7b176e6e0b2f7748585a2ee783a1f8a
Alex Bumbu <alex.bumbu@8x8.com>
2021-06-04 12:22:14 +0300

    ios: add screen sharing example

---
+++ ios/swift-screensharing/JitsiSDKScreenSharingTest/JitsiSDKScreenSharingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JitsiSDKScreenSharingTest
+//
+//  Created by Alex-Dan Bumbu on 04.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
