dc30f4485c9dc369a37ab839eb368cd31edec46a
Alex Bumbu <alex.bumbu@8x8.com>
2022-05-17 16:37:18 +0300

    Fix iOS screensharing sample

--- ios/swift-screensharing/JitsiSDKScreenSharingTest/JitsiSDKScreenSharingTest/ViewController.swift
+++ ios/swift-screensharing/JitsiSDKScreenSharingTest/JitsiSDKScreenSharingTest/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
+private enum Segue: String {
+    case showMeeting
+}
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet private var meetingNameInput: UITextField!
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == Segue.showMeeting.rawValue {
+            guard let meetingName = meetingNameInput.text, meetingName.count > 0 else {
+                print("Enter meeting name")
+                return false
+            }
+            
+            return true
+        }
+        
+        return false
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Segue.showMeeting.rawValue {
+            let navController = segue.destination as! UINavigationController
+            let meetingsController = navController.viewControllers.first as! JitsiMeetViewController
+            meetingsController.meetingName = meetingNameInput.text!
+        }
+    }
 }
 
