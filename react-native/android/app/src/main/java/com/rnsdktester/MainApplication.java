14001cd1767b088b6346d08680f07a388e4c38d8
Calin-Teodor <calin.chitu@8x8.com>
2024-03-07 17:54:05 +0200

    react-native: update sample app use latest rnsdk version

--- react-native/android/app/src/main/java/com/rnsdktester/MainApplication.java
+++ react-native/android/app/src/main/java/com/rnsdktester/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.oney.WebRTCModule.WebRTCModuleOptions;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
@@ -52,7 +53,12 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+
+    WebRTCModuleOptions options = WebRTCModuleOptions.getInstance();
+    options.enableMediaProjectionService = true;
+
     SoLoader.init(this, /* native exopackage */ false);
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
