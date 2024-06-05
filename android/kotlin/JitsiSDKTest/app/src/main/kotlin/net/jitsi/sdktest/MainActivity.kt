f496bda912e2cdc1d8674b1961b9744692a6b32c
Vincent TE <vincent.te@infomaniak.com>
2024-06-05 13:21:36 +0200

    Modification to reproduce bug

--- android/kotlin/JitsiSDKTest/app/src/main/kotlin/net/jitsi/sdktest/MainActivity.kt
+++ android/kotlin/JitsiSDKTest/app/src/main/kotlin/net/jitsi/sdktest/MainActivity.kt
@@ -9,12 +9,18 @@ import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import org.jitsi.meet.sdk.*
+import org.jitsi.meet.sdk.BroadcastEvent
+import org.jitsi.meet.sdk.BroadcastIntentHelper
+import org.jitsi.meet.sdk.JitsiMeet
+import org.jitsi.meet.sdk.JitsiMeetActivity
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
 import timber.log.Timber
 import java.net.MalformedURLException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private var serverURL = URL("https://kmeet.infomaniak.com/")
 
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
@@ -26,24 +32,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize default options for Jitsi Meet conferences.
-        val serverURL: URL
-        serverURL = try {
-            // When using JaaS, replace "https://meet.jit.si" with the proper serverURL
-            URL("https://meet.jit.si")
-        } catch (e: MalformedURLException) {
-            e.printStackTrace()
-            throw RuntimeException("Invalid server URL!")
-        }
         val defaultOptions = JitsiMeetConferenceOptions.Builder()
-                .setServerURL(serverURL)
-                // When using JaaS, set the obtained JWT here
-                //.setToken("MyJWT")
-                // Different features flags can be set
-                //.setFeatureFlag("toolbox.enabled", false)
-                //.setFeatureFlag("filmstrip.enabled", false)
-                .setFeatureFlag("welcomepage.enabled", false)
-                .build()
+            .setServerURL(serverURL)
+            .setFeatureFlag("prejoinpage.enabled", false)
+            // When using JaaS, set the obtained JWT here
+            //.setToken("MyJWT")
+            // Different features flags can be set
+            //.setFeatureFlag("toolbox.enabled", false)
+            //.setFeatureFlag("filmstrip.enabled", false)
+            .build()
         JitsiMeet.setDefaultConferenceOptions(defaultOptions)
 
         registerForBroadcastMessages()
@@ -62,11 +59,12 @@ class MainActivity : AppCompatActivity() {
             // Build options object for joining the conference. The SDK will merge the default
             // one we set earlier and this one when joining.
             val options = JitsiMeetConferenceOptions.Builder()
-                    .setRoom(text)
-                    // Settings for audio and video
-                    //.setAudioMuted(true)
-                    //.setVideoMuted(true)
-                    .build()
+                .setServerURL(serverURL)
+                .setRoom(text)
+                // Settings for audio and video
+                //.setAudioMuted(true)
+                //.setVideoMuted(true)
+                .build()
             // Launch the new activity with the given options. The launch() method takes care
             // of creating the required Intent and passing the options.
             JitsiMeetActivity.launch(this, options)
@@ -96,8 +94,16 @@ class MainActivity : AppCompatActivity() {
         if (intent != null) {
             val event = BroadcastEvent(intent)
             when (event.type) {
-                BroadcastEvent.Type.CONFERENCE_JOINED -> Timber.i("Conference Joined with url%s", event.getData().get("url"))
-                BroadcastEvent.Type.PARTICIPANT_JOINED -> Timber.i("Participant joined%s", event.getData().get("name"))
+                BroadcastEvent.Type.CONFERENCE_JOINED -> Timber.i(
+                    "Conference Joined with url%s",
+                    event.getData().get("url")
+                )
+
+                BroadcastEvent.Type.PARTICIPANT_JOINED -> Timber.i(
+                    "Participant joined%s",
+                    event.getData().get("name")
+                )
+
                 else -> Timber.i("Received event: %s", event.type)
             }
         }
@@ -106,6 +112,7 @@ class MainActivity : AppCompatActivity() {
     // Example for sending actions to JitsiMeetSDK
     private fun hangUp() {
         val hangupBroadcastIntent: Intent = BroadcastIntentHelper.buildHangUpIntent()
-        LocalBroadcastManager.getInstance(this.applicationContext).sendBroadcast(hangupBroadcastIntent)
+        LocalBroadcastManager.getInstance(this.applicationContext)
+            .sendBroadcast(hangupBroadcastIntent)
     }
 }
