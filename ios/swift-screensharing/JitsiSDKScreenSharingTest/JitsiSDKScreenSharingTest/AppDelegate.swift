fa536a5be7acec7a3ecbc912ff72a63820f4e03c
Calin-Teodor <calin.chitu@8x8.com>
2026-04-21 15:41:08 +0300

    ios: use latest JitsiMeetSDK

--- ios/swift-screensharing/JitsiSDKScreenSharingTest/JitsiSDKScreenSharingTest/AppDelegate.swift
+++ ios/swift-screensharing/JitsiSDKScreenSharingTest/JitsiSDKScreenSharingTest/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
